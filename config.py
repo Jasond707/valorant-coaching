2527ce6afa11d29c1a232042fddb14b9fa4a0770
Jason D <previde@ymail.com>
2024-09-17 13:03:52 -0700

    Add requirements.txt and update config.py

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
-    # Directly setting the DATABASE_URL for testing purposes
-    SQLALCHEMY_DATABASE_URI = 'postgres://u7u7afd5vrc59s:pbd6e4bcfedc2d38cabc6fff5566268b1e5ea9bbe6a056a7c6a725137464ddef4@c9uss87s9bdb8n.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dddvu8l8cjo5tb'
-    
+    # Using the DATABASE_URL from environment variables if available
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://u7u7afd5vrc59s:pbd6e4bcfedc2d38cabc6fff5566268b1e5ea9bbe6a056a7c6a725137464ddef4@c9uss87s9bdb8n.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dddvu8l8cjo5tb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
