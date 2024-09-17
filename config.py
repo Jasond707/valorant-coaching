0a3192d89283fa62f0a27187c7bf6c1279841f60
Jason D <previde@ymail.com>
2024-09-17 13:23:30 -0700

    Add psycopg2-binary to requirements.txt

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    
-    # Using the DATABASE_URL from environment variables if available
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://u7u7afd5vrc59s:pbd6e4bcfedc2d38cabc6fff5566268b1e5ea9bbe6a056a7c6a725137464ddef4@c9uss87s9bdb8n.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dddvu8l8cjo5tb'
+    # Replace this with your actual Heroku Postgres URL
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://u7u7afd5vrc59s:pbd6e4bcfedc2d38cabc6fff5566268b1e5ea9bbe6a056a7c6a725137464ddef4@c9uss87s9bdb8n.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dddvu8l8cjo5tb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
