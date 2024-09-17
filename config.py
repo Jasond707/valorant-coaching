82843ea4af74ae755f265d359e8fc600d12b07f2
Jason D <previde@ymail.com>
2024-09-16 20:48:47 -0700

    Set DATABASE_URL directly for testing

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
-    # Use the DATABASE_URL environment variable for Heroku Postgres
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # Directly setting the DATABASE_URL for testing purposes
+    SQLALCHEMY_DATABASE_URI = 'postgres://u7u7afd5vrc59s:pbd6e4bcfedc2d38cabc6fff5566268b1e5ea9bbe6a056a7c6a725137464ddef4@c9uss87s9bdb8n.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dddvu8l8cjo5tb'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
