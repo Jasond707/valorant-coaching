9320287ba7a1003aeae780fb998d6298a841b008
Jason D <previde@ymail.com>
2024-09-18 17:18:04 -0700

    Your commit message here

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 import os
+import psycopg2
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    # Retrieve the DATABASE_URL environment variable if set, otherwise fall back to a local SQLite database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///local.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
